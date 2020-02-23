c454173b0af4d31f619c148a84966534913d05eb
=HyeRyeongSong <=loverhdiddl@gmail.com>
2020-02-23 13:19:41 +0900

    crawl hashtag v2

--- hashtagCrawl/main.py
+++ hashtagCrawl/main.py
@@ -8,3 +8,8 @@ cocktailBase = ["Vodka", "Rum", "Liqueurs", "Cognac", "Gin", "Brandy", "Bourbon"
 for oneCocktail in cocktailBase:
     crawling.getInfo(oneCocktail)
 #search = urllib.parse.quote(search)
+
+#"Vodka", "Rum", "Liqueurs", "Cognac", "Gin", "Brandy", "Bourbon", "AmericanWhiskey",\
+#                "Whiskey", "Beer", "Scotch", "Tequila", "Vermouth", "AperitifWine", \
+#                "Amaretto", "Champagne", "SparklingWine", "IrishWhiskey", "RyeWhiskey",\
+#                "Wine", "Absinthe", "RhumAgricole", "Pisco", "Armagnac"
