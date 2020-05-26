4816ce486c2c5556655d8edaf65561cabf74bc7d
co3oing <co3oing@naver.com>
2020-05-27 00:39:56 +0900

    http post!

--- ccookncook.bixbar/code/CreateUser.js
+++ ccookncook.bixbar/code/CreateUser.js
@@ -3,17 +3,17 @@ var console = require('console')
 var config = require('config')
 
 module.exports.function = function createUser () {
-  var User = {
-    "gender": "gender",
-    "age": "age",
-    "userId" : $vivContext.bixbyUserId,
+  var user = {
+    "gender": "femail",
+    "age": "23",
+    "userId" : "testID",//$vivContext.bixbyUserId,
   };
   var options = {
     passAsJson: true,
     returnHeaders: true,
     format: 'json'
   };
-  var response = http.postUrl(config.get('remote.url') + '/shoes', shoe, options);
+  var response = http.postUrl(config.get('remote.url') + '/user', user, options);
 
   console.log(response);
   return response.parsed;
